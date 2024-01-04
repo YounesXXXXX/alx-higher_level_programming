562960ef393249fe14df37aee2fe6eb0b8aef5d2
Younes Ait Lahsen <144718552+YounesXXXXX@users.noreply.github.com>
2024-01-04 23:46:28 +0100

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    sys.argv.pop(0)
-    total = 0
-    for number in sys.argv:
-        total += int(number)
-    print("{}".format(total))
